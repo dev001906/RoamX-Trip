d392dc4b4c462d24a99e96ac383c3f80662c0548
Dev <devendra.sadinsar@gmail.com>
2025-10-21 12:11:24 +0530

    sorted an error in currency_converter

--- utils/currency_converter.py
+++ utils/currency_converter.py
@@ -1,7 +1,7 @@
 import requests
 
 class CurrencyConverter:
-    def __init__(self):
+    def __init__(self,api_key:str):
         self.base_url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/"
 
     def convert(self,amount:float, from_currency:str, to_currency:str):
@@ -10,9 +10,10 @@ class CurrencyConverter:
         response = requests.get(url)
         if response.status_code != 200:
             raise Exception("API call failed: ",response.json())
-        rates = requests.json()["conversion_rates"]
+        rates = response.json()["conversion_rates"]
         if to_currency not in rates:
             raise ValueError(f"{to_currency} not found in exchange rates.")
         return amount * rates[to_currency]
     
-    
+import requests
+
